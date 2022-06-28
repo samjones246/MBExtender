6e20c8a5059259597607861ed6c502cb7eec4754
Sam Jones <sam.jones240699@gmail.com>
2022-06-28 15:32:54 +0100

    fleshed out setters, added playing/loading bools

--- MBExtender/plugins/ASLHelper/ASLHelper.cpp
+++ MBExtender/plugins/ASLHelper/ASLHelper.cpp
@@ -21,9 +21,15 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //-----------------------------------------------------------------------------
-
+// ASLHelper
+// Makes it more convenient for an ASL autosplitter to interface with the game by
+// storing useful values in low level statics rather than torquescript level variables.
+//-----------------------------------------------------------------------------
 #include <MBExtender/MBExtender.h>
 #include <TorqueLib/console/console.h>
+#include <string>
+
+using namespace std;
 
 MBX_MODULE(ASLHelper);
 
@@ -36,33 +42,73 @@ bool initPlugin(MBX::Plugin &plugin)
 class ASLHelper {
     public:
         static int state;
-        static int levelid;
-        static int leveltime;
-        static int totaltime;
+        static string levelName;
+        static bool playing;
+        static bool loading;
+        static int levelTimeMillis;
+        static int totalTimeMillis;
 };
 
 int ASLHelper::state = 0;
-int ASLHelper::levelid = 0;
-int ASLHelper::leveltime = 0;
-int ASLHelper::totaltime = 0;
+string ASLHelper::levelName = "";
+bool ASLHelper::playing = false;
+bool ASLHelper::loading = false;
+int ASLHelper::levelTimeMillis = 0;
+int ASLHelper::totalTimeMillis = 0;
 
 MBX_CONSOLE_FUNCTION(ASL_setState, void, 2, 2, "ASL_setState(state)")
 {
-    ASLHelper::state = atoi(argv[1]);
+    int state = 0;
+    if (strcmp(argv[1], "start") == 0) {
+        state = 1;
+    } else if (strcmp(argv[1], "go") == 0) {
+        state = 2;
+    } else if (strcmp(argv[1], "end") == 0) {
+        state = 3;
+    }
+    ASLHelper::state = state;
+    TGE::Con::printf("[ASLHelper] State set to %s (%d)", argv[1], state);
 }
 
-MBX_CONSOLE_FUNCTION(ASL_setLevelId, void, 2, 2, "ASL_setLevelId(levelid)")
+MBX_CONSOLE_FUNCTION(ASL_setLevelName, void, 2, 2, "ASL_setLevelName(levelName)")
 {
-    ASLHelper::levelid = atoi(argv[1]);
+    ASLHelper::levelName = argv[1];
+    TGE::Con::printf("[ASLHelper] Level ID set to %s", argv[1]);
 }
 
-MBX_CONSOLE_FUNCTION(ASL_setLevelTime, void, 2, 2, "ASL_setLevelTime(leveltime)")
+MBX_CONSOLE_FUNCTION(ASL_setPlaying, void, 2, 2, "ASL_setPlaying(playing)")
 {
-    ASLHelper::leveltime = atoi(argv[1]);
+    bool playing = false;
+    if (strcmp(argv[1], "true") == 0 || atoi(argv[1]) == 1) {
+        playing = true;
+    }
+    ASLHelper::playing = playing;
+    TGE::Con::printf("[ASLHelper] Playing set to %s", argv[1]);
 }
 
-MBX_CONSOLE_FUNCTION(ASL_setTotalTime, void, 2, 2, "ASL_setTotalTime(totaltime)")
+MBX_CONSOLE_FUNCTION(ASL_setLoading, void, 2, 2, "ASL_setLoading(loading)")
 {
-    ASLHelper::totaltime = atoi(argv[1]);
+    bool loading = false;
+    if (strcmp(argv[1], "true") == 0 || atoi(argv[1]) == 1) {
+        loading = true;
+    }
+    ASLHelper::loading = loading;
+    TGE::Con::printf("[ASLHelper] Loading set to %s", argv[1]);
+}
+
+MBX_CONSOLE_FUNCTION(ASL_setLevelTimeMillis, void, 2, 2, "ASL_setLevelTimeMillis(levelTimeMillis)")
+{
+    int levelTimeMillis = 0;
+    levelTimeMillis = atoi(argv[1]);
+    ASLHelper::levelTimeMillis = levelTimeMillis;
+    TGE::Con::printf("[ASLHelper] Level time set to %d", levelTimeMillis);
+}
+
+MBX_CONSOLE_FUNCTION(ASL_setTotalTimeMillis, void, 2, 2, "ASL_setTotalTimeMillis(totalTimeMillis)")
+{
+    int totalTimeMillis = 0;
+    totalTimeMillis = atoi(argv[1]);
+    ASLHelper::totalTimeMillis = totalTimeMillis;
+    TGE::Con::printf("[ASLHelper] Total time set to %d", totalTimeMillis);
 }
 
